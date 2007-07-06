47b5a249e7ae877d7816504fec4f987442d07931
Martin-Éric Racine <q-funk@iki.fi>
2007-07-06 16:59:51 -0600

    An improved autogen.sh script

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/sh
 
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
@@ -6,7 +6,15 @@ test -z "$srcdir" && srcdir=.
 ORIGDIR=`pwd`
 cd $srcdir
 
+rm -rf config.guess config.sub ltmain.sh
+
 autoreconf -v --install || exit 1
 cd $ORIGDIR || exit $?
 
 $srcdir/configure --enable-maintainer-mode "$@"
+
+make distclean
+
+rm -rf autom4te.cache
+
+#EOF
