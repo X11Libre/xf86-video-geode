8ce4c5cc9650ddc81d9243bc416522800bce3afc
Martin-Éric Racine <q-funk@iki.fi>
2007-07-25 09:55:25 -0600

    Add the all-important if/then loop to avoid squashing the ChangeLog if autogen.sh is executed outside of a git tree.

--- autogen.sh
+++ autogen.sh
@@ -11,7 +11,10 @@ rm -rf config.guess config.sub ltmain.sh
 autoreconf -v --install || exit 1
 cd $ORIGDIR || exit $?
 
-$srcdir/missing --run git-log --stat | fmt --split-only 1> ChangeLog 2>/dev/null
+if [ -d .git ]
+then
+	$srcdir/missing --run git-log --stat | fmt --split-only 1> ChangeLog
+fi
 
 $srcdir/configure --enable-maintainer-mode "$@"
 
